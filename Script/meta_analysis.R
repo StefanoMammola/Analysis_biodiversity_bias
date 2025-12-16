49e58f1bf916e88e61800e1fc2da907b5b888bd5
Stefano Mammola <68010284+StefanoMammola@users.noreply.github.com>
2025-12-16 14:46:22 +0100

    Updated script

--- Script/meta_analysis.R
+++ Script/meta_analysis.R
@@ -36,6 +36,9 @@ db <- read_excel("Data/Database_meta_analysis_bias.xlsx", na = "NA") |>
   dplyr::arrange(Pearson_r) 
 str(db)
 
+#Distinct db
+db2 <- db |> dplyr::distinct(ID, .keep_all = TRUE)
+
 ####################################################################################
 # Data exploration -----------------------------------------------------------------
 ####################################################################################
@@ -49,11 +52,44 @@ nrow(db) #881
 #Number of journals
 nlevels(db$Journal) #78
 
+#Publication by year
+db2 |>
+  ggplot2::ggplot(aes(Year))+
+  geom_bar(fill = "#2c7fb8", color = "black")+
+  labs(
+    x = "",
+    y = "Number of publications"
+  ) +
+  scale_x_continuous(breaks = seq(min(db2$Year), max(db2$Year), by = 2)) +
+  scale_y_continuous(breaks = seq(0, 12, by = 2)) +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))
+
+#Studies by country
+table(db2$Geography_macro) |>
+  data.frame() |>
+  ggplot2::ggplot(aes(x = Freq, y = Var1))+
+  geom_col(fill = "#2c7fb8")+
+  labs(
+    y = "",
+    x = "Number of publications"
+  )
+
+#Studies by country
+table(db2$Study_type) |>
+  data.frame() |>
+  ggplot2::ggplot(aes(x = Freq, y = Var1))+
+  geom_col(fill = "#2c7fb8")+
+  labs(
+    y = "",
+    yx = "Number of publications"
+  )
+
 #Summary of estimates in each combination of response and predictor
 (table_tot <- table(db$Dependent_zoomed,db$Indipendent_macro))
 
 # Derive Fischer's Z and its variance -------------------------------------
-db <- escalc(measure = "ZCOR", ri = Pearson_r, ni = N, data = db)
 
+db$Pearson_r <- pmin(pmax(db$Pearson_r, -0.999), 0.999)
+db <- metafor::escalc(measure = "ZCOR", ri = Pearson_r, ni = N, data = db)
 
 
